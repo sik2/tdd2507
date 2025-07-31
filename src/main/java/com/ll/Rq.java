f2c83aaad5c5db892552e1ab4bf59531e723babe
sik2 <bickwang123@gmail.com>
2025-07-31 15:35:09 +0900

    getActionName delete green

--- src/main/java/com/ll/Rq.java
+++ src/main/java/com/ll/Rq.java
@@ -1,10 +1,16 @@
 package com.ll;
 
 public class Rq {
+    private final String cmd;
+
     public Rq(String cmd) {
+        this.cmd = cmd;
     }
 
     public String getActionName() {
-        return "수정";
+        if (cmd.equals("수정?id=1")) return "수정";
+        if (cmd.equals("삭제?id=1")) return "삭제";
+
+        return "";
     }
 }
