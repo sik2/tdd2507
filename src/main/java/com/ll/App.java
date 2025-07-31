cf8f572600574d0d610c5ec12ae7e81aa132d5b1
sik2 <bickwang123@gmail.com>
2025-07-31 14:44:05 +0900

    plus red

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -4,4 +4,8 @@ public class App {
     public void run () {
         System.out.println("==계산기==");
     }
+
+    int plus(int a, int b) {
+        return 0;
+    }
 }
--- src/test/java/com/ll/AppTest.java
+++ src/test/java/com/ll/AppTest.java
@@ -3,10 +3,15 @@ package com.ll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AppTest {
     @Test
     @DisplayName("테스트 1")
     void t1() {
+        App app = new App();
+        int rs = app.plus(1, 2);
 
+        assertEquals(3, rs);
     }
 }
